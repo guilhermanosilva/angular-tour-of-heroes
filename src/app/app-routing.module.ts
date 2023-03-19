5a73a5d95fc9fce39e2bb445464c1cc0b7b36dce
Guilhermano Silva <guilhermanodev@gmail.com>
2023-03-19 09:39:38 -0300

    feat: create the heroes router navigation

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
